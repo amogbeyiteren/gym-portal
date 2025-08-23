92483d124e2e6ec7b00f797487a86cbd24895c6e
0daysecurity <amogbeyiteren@gmail.com>
2025-08-23 10:48:54 +0100

    Update client.module.ts

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { EmailModule } from 'src/email/email.module';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
 import { AuthModule } from '../auth/auth.module';
 import { FileUploadModule } from '../file-upload/file-upload.module';
 
 @Module({
-  imports: [AuthModule, FileUploadModule],
+  imports: [AuthModule, FileUploadModule, DatabaseModule, EmailModule],
   controllers: [ClientController],
   providers: [ClientService],
   exports: [ClientService],
